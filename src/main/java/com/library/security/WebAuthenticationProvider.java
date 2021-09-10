93681f16e665cc6b1937c1032f828aab9740f686
ViktoriyaRyazhska <pavlo.fedik14@gmail.com>
2021-09-10 16:44:29 +0300

    v

---
+++ src/main/java/com/library/security/WebAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.library.security;
+
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class WebAuthenticationProvider implements AuthenticationProvider {
+
+    private UserDetailsService userService;
+    private PasswordEncoder passwordEncoder;
+
+    public WebAuthenticationProvider(UserDetailsService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @SneakyThrows
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    userDetails.getPassword(),
+                    userDetails.getAuthorities()
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
